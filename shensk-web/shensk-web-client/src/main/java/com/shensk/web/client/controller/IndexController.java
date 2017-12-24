2e979755b30d2df3fc302c2c691b66d07257a162
shensk <164463450@qq.com>
2017-12-24 12:14:22 +0800

    add shensk-common

--- shensk-web/shensk-web-client/src/main/java/com/shensk/web/client/controller/IndexController.java
+++ shensk-web/shensk-web-client/src/main/java/com/shensk/web/client/controller/IndexController.java
@@ -1,19 +1,20 @@
 package com.shensk.web.client.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.shensk.common.base.BaseController;
 import com.shensk.web.rpc.api.EchoService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class IndexController {
+public class IndexController extends BaseController{
 
     @Reference(version = "1.0.0")
     private EchoService echoService;
 
     @RequestMapping(value = "/index",method = RequestMethod.GET)
     public void index(){
-        System.out.println(echoService.echo("张三"));
+        System.out.println(thymeleaf("/ref"));
     }
 }
