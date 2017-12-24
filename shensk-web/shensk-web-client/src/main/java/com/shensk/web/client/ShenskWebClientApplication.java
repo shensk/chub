56c43353661b447482cb2543e39d1e6b2669f498
shensk <164463450@qq.com>
2017-12-24 12:16:29 +0800

    edit

--- shensk-web/shensk-web-client/src/main/java/com/shensk/web/client/ShenskWebClientApplication.java
+++ shensk-web/shensk-web-client/src/main/java/com/shensk/web/client/ShenskWebClientApplication.java
@@ -1,11 +1,7 @@
 package com.shensk.web.client;
 
-import com.alibaba.dubbo.config.annotation.Reference;
-import com.shensk.web.rpc.api.EchoService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class ShenskWebClientApplication {
