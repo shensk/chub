aff22677b8320eff293cf52fb3725d0fb2a214b7
164463450 <164463450@qq.com>
2017-12-25 13:41:22 +0800

    update pom

---
+++ shensk-web/shensk-web-rpc-service/src/main/java/com/shensk/dao/mapper/UserMapper.java
@@ -0,0 +1,19 @@
+package com.shensk.dao.mapper;
+
+import com.shensk.dao.model.User;
+
+import java.util.List;
+
+public interface UserMapper {
+
+    List<User> getAll();
+
+    User getOne(Long id);
+
+    void insert(User user);
+
+    void update(User user);
+
+    void delete(Long id);
+
+}
--- shensk-web/shensk-web-rpc-service/src/main/java/com/shensk/web/rpc/service/impl/EchoServerImpl.java
+++ shensk-web/shensk-web-rpc-service/src/main/java/com/shensk/web/rpc/service/impl/EchoServerImpl.java
@@ -1,12 +1,20 @@
 package com.shensk.web.rpc.service.impl;
 
+import com.shensk.dao.mapper.UserMapper;
 import com.shensk.web.rpc.api.EchoService;
 import com.alibaba.dubbo.config.annotation.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @Service(version = "1.0.0")
 public class EchoServerImpl implements EchoService{
+
+    @Autowired
+    private UserMapper userMapper;
+
     public String echo(String str) {
-        return "Hello " + str;
+//        User User = userMapper.getOne(1L);
+        return "hello" + str;
     }
 }
