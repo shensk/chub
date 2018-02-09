fe3ab307c628cecae7c676b74614936c58256c40
shensk <164463450@qq.com>
2018-02-09 22:55:37 +0800

    #配合nginx前后端分离

--- shensk-web/shensk-web-client/src/main/java/com/shensk/web/client/controller/IndexController.java
+++ shensk-web/shensk-web-client/src/main/java/com/shensk/web/client/controller/IndexController.java
@@ -3,22 +3,20 @@ package com.shensk.web.client.controller;
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.shensk.common.base.BaseController;
 import com.shensk.web.rpc.api.EchoService;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class IndexController extends BaseController{
 
     @Reference(version = "1.0.0")
     private EchoService echoService;
 
     @RequestMapping(value = "/index",method = RequestMethod.GET)
-    public ModelAndView index(ModelAndView modelAndView){
+    public String index(){
         String re = echoService.echo("ssk");
         System.out.println(re);
-        modelAndView.setViewName("/index");
-        return modelAndView;
+        return "/index";
     }
 }
